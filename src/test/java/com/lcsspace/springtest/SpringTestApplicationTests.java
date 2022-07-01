bc2f0371e4e34a1ae4e5615ebf74de5062cbdb7a
lcs <sennyboy@163.com>
2022-07-01 11:10:05 +0800

    RESTful api test and using juint testing code

---
+++ src/test/java/com/lcsspace/springtest/SpringTestApplicationTests.java
@@ -0,0 +1,83 @@
+package com.lcsspace.springtest;
+
+import com.lcsspace.springtest.web.UserController;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class SpringTestApplicationTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+    }
+
+    @Test
+    public void testUserController() throws Exception {
+        // 测试UserController
+        RequestBuilder request;
+
+        //1. get查询user列表
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+        //2. post提交一个user
+        request = post("/users/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\":1, \"name\":\"jjb\", \"age\":99}");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        //3. get获取user列表
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        equalTo("[{\"id\":1,\"name\":\"jjb\",\"age\":99}]")));
+
+        //4. put修改id为1的user
+        request = put("/users/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"putjjb\",\"age\":30}");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        // 5. get一个id为1的user
+        request = get("/users/1");
+        mvc.perform(request)
+                .andExpect(content().string(
+                        equalTo("{\"id\":1,\"name\":\"putjjb\",\"age\":30}")));
+
+        //6. deleteid为1的user
+        request = delete("/users/1");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        //7. get查询user列表
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+    }
+}
